35228288bd86154390fd6bc7ff5d6c232980d1e5
Tom Leonardsson <tom.leo@hotmail.com>
2015-01-25 00:41:19 +0100

    Started working on the lava object

---
+++ OneManCoOp/OneManCoOp/OneManCoOp/Lava.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace OneManCoOp
+{
+    class Lava : GameObject
+    {
+        public Lava(Vector2 position2)
+        {
+            Position = position2;
+            Sprite = new Sprite(TextureManager.door, Position, new Vector2(32, 32), 4, new Point(32, 32), 0.2f);
+        }
+        public override void Update()
+        {
+            if(Game1.player.Hitbox.Intersects(Hitbox))
+            {
+
+            }
+        }
+    }
+}
