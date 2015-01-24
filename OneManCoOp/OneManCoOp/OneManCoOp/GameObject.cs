beda39e36f54efafaa2b0abed8ab8de0d7278748
Johannes-Larsson <mrja1lisepic@gmail.com>
2015-01-24 17:43:46 +0100

    made gameObject.position public set

--- OneManCoOp/OneManCoOp/OneManCoOp/GameObject.cs
+++ OneManCoOp/OneManCoOp/OneManCoOp/GameObject.cs
@@ -11,7 +11,7 @@ namespace OneManCoOp
     {
         float health;
 
-        public Vector2 Position { get { return Sprite.Position; } protected set { Sprite.Position = value; } }
+        public Vector2 Position { get { return Sprite.Position; } set { Sprite.Position = value; } }
         public Vector2 Size { get { return Sprite.Size; } protected set { Sprite.Size = value; } }
         public Vector2 Center { get { return new Vector2(Hitbox.Center.X, Hitbox.Center.Y); } }
         public virtual Rectangle Hitbox { get { return Sprite.Area; } }
