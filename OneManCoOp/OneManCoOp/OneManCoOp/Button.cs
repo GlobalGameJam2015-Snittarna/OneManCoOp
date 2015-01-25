0b9266b86bf677a85dcac2838c90b2b7734ef039
Tom Leonardsson <tom.leo@hotmail.com>
2015-01-25 07:49:31 +0100

    Removed test texrt

--- OneManCoOp/OneManCoOp/OneManCoOp/Button.cs
+++ OneManCoOp/OneManCoOp/OneManCoOp/Button.cs
@@ -68,10 +68,5 @@ namespace OneManCoOp
                 RemovePress = false;
         }
 
-        public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
-        {
-            spriteBatch.DrawString(TextureManager.font, Tag.ToString(), Position, Color.White);
-            base.Draw(spriteBatch);
-        }
     }
 }
